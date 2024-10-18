399b7ab816913713ebc2d3fda9a71408ef7eb3b2
ANKITHKUMAR <142873258+ankithkumar08@users.noreply.github.com>
2024-10-18 18:24:25 +0530

    Add files via upload

---
+++ python/day4.py
@@ -0,0 +1,17 @@
+#Number as Positive, Negative, or Zero
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+
+#Number is Odd or Even
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print(f"{num} is Even")
+else:
+   print(f"{0} is Odd")
