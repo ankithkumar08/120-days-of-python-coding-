b205500f38a0d0f068cf3a89d56443ca7b164f1b
ANKITHKUMAR <142873258+ankithkumar08@users.noreply.github.com>
2024-10-18 18:23:28 +0530

    Add files via upload

---
+++ python/day3.py
@@ -0,0 +1,18 @@
+#adding Two Numbers
+
+def adding(sum):
+    sum = a + b
+    return sum
+a = int(input("enter the a value:"))
+b = int(input("enter the b value:"))
+result = adding(sum)
+print(f"the sum of {a} and{b} is {result}")
+
+#Find the Square Root
+
+def square(num):
+    square = num ** 0.5
+    return square
+num = int(input("Enter the number to check the sqare root:"))
+result = square(num)
+print(f"the square root of the {num} is the {result}")
