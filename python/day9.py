06307561d60240950d1be1afa7601d2974db6773
ANKITHKUMAR <142873258+ankithkumar08@users.noreply.github.com>
2024-10-23 19:02:41 +0530

    Add files via upload

---
+++ python/day9.py
@@ -0,0 +1,14 @@
+#Check Armstrong number of n digits
+
+num = int(input("Enter the number to check the armstrog number :"))
+order = len(str(num))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
