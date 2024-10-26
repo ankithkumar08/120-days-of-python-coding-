fa5341758b546b747b48220481847d3179d1b27f
ANKITHKUMAR <142873258+ankithkumar08@users.noreply.github.com>
2024-10-26 16:38:48 +0530

    Delete python/day10.py

--- python/day10.py
+++
@@ -1,6 +0,0 @@
-# Multiplication table (from 1 to 10) in Python
-
-num = int(input("Wchich multiplication table Do You Want TO DisplayS? "))
-
-for i in range(1, 11):
-   print(num, 'x', i, '=', num*i)
