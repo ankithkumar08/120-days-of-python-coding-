6a9be68a1207669a5b7ec62b5e0a1a42926ee479
ANKITHKUMAR <142873258+ankithkumar08@users.noreply.github.com>
2025-03-09 12:18:56 +0530

    Update day_30.py

--- python/day_30.py
+++ python/day_30.py
@@ -1,16 +1,17 @@
+#method1
 my_list = []
 if not len(my_list):
     print("the list is empty")
 else:
     print("the list is not empty")
 
-
+#method2
 my_list = []
 if not len(my_list):
     print("the list is empty")
 else:
     print("the list is not empty")
-    
+#method3
 my_list = []
 if my_list == []:
     print("The list is empty")
